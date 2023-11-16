872a09523f6f004cbb81519c81672a720137c4ca
Roberto Manfreda <robertomanfreda1994@gmail.com>
2023-11-16 16:48:22 +0100

    moved example values to t_utils.h;

--- ctest/t_rc_list.c
+++ ctest/t_rc_list.c
@@ -7,39 +7,6 @@
 #include "rc_list.h"
 #include "t_utils.h"
 
-static long long llData1 = 10;
-static long long llData2 = 20;
-static long long llData3 = 30;
-static long long llData4 = 40;
-static long long llData5 = 50;
-
-static long lData1 = 10;
-static long lData2 = 20;
-static long lData3 = 30;
-static long lData4 = 40;
-static long lData5 = 50;
-
-static float fData1 = 3.14f;
-static float fData2 = 3.15f;
-static float fData3 = 3.16f;
-static float fData4 = 3.17f;
-static float fData5 = 3.18f;
-
-static bool bDataTrue = true;
-static bool bDataFalse = false;
-
-static const char *sData1 = "sData1";
-static const char *sData2 = "sData2";
-static const char *sData3 = "sData3";
-static const char *sData4 = "sData4";
-static const char *sData5 = "sData5";
-
-static char cData1 = 'a';
-static char cData2 = 'b';
-static char cData3 = 'c';
-static char cData4 = 'd';
-static char cData5 = 'e';
-
 void test_list_create() {
     printf("Executing: %s\n", __func__);
 
